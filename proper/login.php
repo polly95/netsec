4145d9de5248e8844c79e793167095e4b4cfedbc
Polly <polena.lilyanova@gmail.com>
2018-01-11 20:23:36 +0200

    Same password and username i hope

--- proper/login.php
+++ proper/login.php
@@ -2,7 +2,7 @@
       if(isset($_GET['password']) && isset($_GET['username'])) {
         $username = $_GET['username']; $password = $_GET['password'];
         
-        if($username === 'heartbleed' && $password === 'difficultPassword'){
+        if($username === 'bedbugs' && $password === 'difficultPassword'){
           $_SESSION['login'] = true; header('LOCATION:failure.php'); die();
         } {
           header('LOCATION:error.php'); die();
--- script/req.sh
+++ script/req.sh
@@ -2,7 +2,7 @@
 
 while true
 do
-	wget --no-check-certificate "https://192.168.1.10/login.php?username=heartbleed&password=difficultPass" #vulnerable server
-	# wget --no-check-certificate "https://192.168.1.11/login.php?username=heartbleed&password=difficultPass" #healthy server
+	wget --no-check-certificate "https://192.168.1.10/login.php?username=bedbugs&password=difficultPassword" #vulnerable server
+	# wget --no-check-certificate "https://192.168.1.11/login.php?username=bedbugs&password=difficultPassword" #healthy server
 	sleep 2m
 done
