0a387924b0e4aedd637c74c2294ec613785d2a87
Polly <polena.lilyanova@gmail.com>
2018-01-11 20:46:03 +0200

    Added clean up

--- script/req.sh
+++ script/req.sh
@@ -4,5 +4,6 @@ while true
 do
 	wget --no-check-certificate "https://192.168.1.10/login.php?username=bedbugs&password=difficultPassword" #vulnerable server
 	# wget --no-check-certificate "https://192.168.1.11/login.php?username=bedbugs&password=difficultPassword" #healthy server
+	rm login.*
 	sleep 2m
 done
